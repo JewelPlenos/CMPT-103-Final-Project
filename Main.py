9ebcc754fcebf9757b619f338ed332fcf12bf129
Jewel Plenos <jewelplenos0@gmail.com>
2025-03-24 15:45:03 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,23 @@
-# Load route data 
 print('Edmonton Transit System') 
-print('---------------------------')
+print('---------------------------------')
+print('(1) Load route data\n(2) Load shapes data\n(3) Reserved for future use\n\n(4) Print shape IDs for a route\n(5) Print coordinates for a shape ID\n(6) Reserved for future use\n\n(7) Save routes and shapes in a pickle\n(8) Load routes and shapes from a pickle\n\n(9) Reserved for future use\n(0) Quit\n\nEnter Command: 0\n')
+
+# Load route data
+def load_route(filename):
+    route_dict = {}
+    with open(filename, 'r') as file: 
+        for line in file: 
+            parts = line.split(',') 
+            route_dict[parts[0]] = parts[3]
+    return route_dict
+
 # Load shapes data
 
 # Reserved for future use (Milestone 2)
-print('hi')
+
+# Main 
+def main(): 
+    route_data = load_route('routes.txt')
+
+if __name__ == '__main__':
+    main()
